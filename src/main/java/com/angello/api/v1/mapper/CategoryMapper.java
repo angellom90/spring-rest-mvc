bac7b5635b239f7a3fbc2fd6cec792093b978fb9
Angello Manrique <angellom90@gmail.com>
2017-11-15 12:53:31 +0100

    adding category service layer

--- src/main/java/com/angello/api/v1/mapper/CategoryMapper.java
+++ src/main/java/com/angello/api/v1/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package com.angello.api.v1.mapper;
 import com.angello.domain.Category;
 import com.angello.api.v1.model.CategoryDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 /**
@@ -13,6 +14,6 @@ public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
-
+//    @Mapping(source = "id",target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
---
+++ src/main/java/com/angello/services/CategoryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.angello.services;
+
+import com.angello.api.v1.mapper.CategoryMapper;
+import com.angello.api.v1.model.CategoryDTO;
+import com.angello.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by jt on 9/26/17.
+ */
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryMapper categoryMapper;
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryMapper categoryMapper, CategoryRepository categoryRepository) {
+        this.categoryMapper = categoryMapper;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<CategoryDTO> getAllCategories() {
+
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::categoryToCategoryDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDTO getCategoryByName(String name) {
+        return categoryMapper.categoryToCategoryDTO(categoryRepository.findByName(name));
+    }
+}
